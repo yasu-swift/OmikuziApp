839bac17a5c59930402e2a45cc31e2f3579c3377
yasu-swift <sp4c72q9@icloud.com>
2020-09-14 20:56:50 +0900

    おみくじ途中

---
+++ OmikuziApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OmikuziApp
+//
+//  Created by 高橋康之 on 2020/09/14.
+//  Copyright © 2020 yasu.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
